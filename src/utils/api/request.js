273d9cd408ab4fc79128f3be0a791184308d164c
Admin <dinhtrieu.15062002@gmail.com>
2023-12-15 08:03:55 +0700

    update requestt

--- src/utils/api/request.js
+++ src/utils/api/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 let baseURL;
 
 if (process.env.NODE_ENV === "production") {
-  baseURL = "https://tame-pink-pronghorn-cape.cyclic.app";
+  baseURL = "https://attractive-rose-llama.cyclic.app";
 } else {
   baseURL = "http://localhost:8000";
 }
